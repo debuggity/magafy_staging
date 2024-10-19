bc988836ed9eaf511dba21a0a1aad1cecd582174
debuggity <sheepbutter@gmail.com>
2024-10-18 20:24:43 -0400

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -818,7 +818,7 @@ function drawLaser(laser, context) {
 function drawLaserCenter(laser, context) {
   const centerX = laser.x + laser.width / 2;
   const centerY = laser.y + laser.height / 2;
-  const radius = laser.width * 0.05;
+  const radius = laser.width * 0.058;
 
   context.save();
   context.translate(centerX, centerY);
